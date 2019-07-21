15b5c8675794260c6a740023e2caf485102d611f
reancodes <33007389+reancodes@users.noreply.github.com>
2019-07-21 16:39:52 +0200

    Udgivelse

---
+++ [dn]/dn_biler/__resource.lua
@@ -0,0 +1,31 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    '2014rs5contentunlocks.meta',
+    'm3e30contentunlocks.meta',
+    'ctrrufcontentunlocks.meta',
+    'mlbrabuscontentunlocks.meta',
+    'teslamodelscontentunlocks.meta',
+    'rs7contentunlocks.meta',
+    'vclasscontentunlocks.meta',
+    'rs6contentunlocks.meta',
+    'm5e60contentunlocks.meta'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file '2014RS5CONTENTUNLOCKS_FILE' '2014rs5contentunlocks.meta'
+data_file 'M3E30CONTENTUNLOCKS_FILE' 'm3e30contentunlocks.meta'
+data_file 'CTRRUFCONTENTUNLOCKS_FILE' 'ctrrufcontentunlocks.meta'
+data_file 'MLBRABUSCONTENTUNLOCKS_FILE' 'mlbrabuscontentunlocks.meta'
+data_file 'TESLAMODELSCONTENTUNLOCKS_FILE' 'teslamodelscontentunlocks.meta'
+data_file 'RS7CONTENTUNLOCKS_FILE' 'rs7contentunlocks.meta'
+data_file 'VCLASSCONTENTUNLOCKS_FILE' 'vclasscontentunlocks.meta'
+data_file 'RS6CONTENTUNLOCKS_FILE' 'rs6contentunlocks.meta'
+data_file 'M5E60CONTENTUNLOCKS_FILE' 'm5e60contentunlocks.meta'
